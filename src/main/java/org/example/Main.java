8755101ad5b487238fadaf3ae9bb965980999c3c
Syntax <instructor@syntaxtechs.com>
2025-02-18 21:30:10 -0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,12 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+         I am Grooot
+  I am Grooot
+  I am Grooot
+               I am Grooot  I am Grooot  I am Grooot  I am Grooot
+               I am Grooot
+               I am Grooot
         
     }
 }
